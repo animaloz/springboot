1595d2b4f4a4075a3ed2c7888041ee6a7dd8a1b7
animaloz <467376747@qq.com>
2018-01-07 14:30:25 +0800

    test commit test jenkins poll

--- src/main/java/com/example/demo/BookController.java
+++ src/main/java/com/example/demo/BookController.java
@@ -19,11 +19,9 @@ public class BookController {
     @RequestMapping(value={""}, method= RequestMethod.GET)
     public List<Book> getBook() {
         List<Book> book = new ArrayList<>(books.values());
+        for (Book bookEntity: book) {
 
-
-
-
-
+        }
         return book;
     }
 
