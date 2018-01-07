c83dfd3192545614f6b6e497c973eb37702c3820
animaloz <467376747@qq.com>
2018-01-07 21:42:07 +0800

    test commit test jenkins poll

--- src/test/java/com/example/demo/WebLayerTest.java
+++ src/test/java/com/example/demo/WebLayerTest.java
@@ -8,13 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @RunWith(SpringRunner.class)
 @WebMvcTest(ConstantsController.class)
 @AutoConfigureRestDocs(outputDir = "target/snippets")
